366db857ef80592fa45d29f809cdeec5b6ae3f99
sarowar-al-amin <sajib.sarowaralamin123@gmail.com>
2020-12-26 12:15:45 +0600

    Dynamic header and footer

---
+++ footer.php
@@ -0,0 +1,107 @@
+    <!------Initaial point of footer part---->
+    <div class="container_full footer_bg maintainSpace">
+        <div class="fix container footer_head">
+
+            <div class="footer_col">
+                <div class="bar_wrap">
+                    <div class="f_bar_y"></div>
+                    <div class="f_bar_b"></div>
+                </div>
+                <br>
+                <h3>About site</h3>
+                <h2>Name and logo</h2>
+                <div class="footer_p">
+                    <p>Lorem Ipsum is simply dummy text of the printing and industry. Lorem Ipsum has been the industry's
+                    </p>
+                    <!-------I don't have any idea is it botton or link------->
+                    <a href="#">Read more</a>
+                </div>
+                <input type="email" placeholder="Enter your email">
+            </div>
+            <div class="footer_col">
+                <div class="bar_wrap">
+                    <div class="f_bar_y"></div>
+                    <div class="f_bar_b"></div>
+                </div>
+                <br>
+                <h3>Our Services</h3>
+                <li><a href="#">Data entry</a></li>
+                <li><a href="#">Graphics design</a></li>
+                <li><a href="#">SEO</a></li>
+                <li><a href="#">Skip Tracing</a></li>
+                <li><a href="#">Lead generation</a></li>
+            </div>
+
+            <div class="footer_col">
+                <div class="bar_wrap">
+                    <div class="f_bar_y"></div>
+                    <div class="f_bar_b"></div>
+                </div>
+                <br>
+                <h3>Quick links</h3>
+                <li><a href="#">About us</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">Contact us</a></li>
+                <li><a href="#">Refund Policy</a></li>
+            </div>
+            <div class="footer_col">
+                <div class="bar_wrap">
+                    <div class="f_bar_y"></div>
+                    <div class="f_bar_b"></div>
+                </div>
+                <br>
+                <h3>Contact Us</h3>
+                <p><i class="fas fa-map-marker-alt"></i> Master Para, Chartolarmor, Rangpur</p>
+                <p><i class="fas fa-envelope"></i> adimin@admin.com</p>
+                <p><i class="fas fa-phone-alt"></i> +0123456789</p>
+            </div>
+
+        </div>
+        <!------Bottom part of the footer------>
+        <div class="white_bar_at_footer"></div>
+        <div class="fix container">
+            <div id="footer_bottom">
+                <p>ItMarket24 &copy; All Reaserved</p>
+                <p>Privacy Terms and Conditions Contact</p>
+                <span>
+                    <a href="#">
+                        <i class="fab fa-facebook"></i>
+                    </a>
+                </span>
+                <span>
+                    <a href="#">
+                        <i class="fab fa-twitter"></i>
+                    </a>
+                </span>
+                <span>
+                    <a href="#">
+                        <i class="fab fa-linkedin"></i>
+                    </a>
+                </span>
+                <span>
+                    <a href="#">
+                        <i class="fab fa-pinterest"></i>
+                    </a>
+                </span>
+            </div>
+        </div>
+
+        <!------ Ending point of footer------>
+
+        <!---------- JavaScript --------->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="jss/vendor-animated-circle.js"></script>
+        <script src="jss/main.js"></script>
+        <!-----Javascript for circle ---->
+        <script src="jss/progresscircle.js"></script>
+        <script>
+            $('.circlechart').circlechart(); // Initialization
+        </script>
+    </div>
+
+    <?php
+        wp_footer();
+    ?>
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,74 @@
+<!-------------
+ This code is written by Md. Sarowar Al-Amin
+---------->
+
+
+<!DOCTYPE html>
+<html <?php language_attributes();?>>
+
+<head>
+    <meta charset="<?php bloginfo('charset');?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php bloginfo('name');?></title>
+    <!------- Those style code only work when internet connection are available -->
+    <script src='https://kit.fontawesome.com/a076d05399.js'></script>
+    <!-------------Other thing  to include ------>
+    <link rel="stylesheet" href="css/modalbox.min.css" type="text/css" />
+    <!-- <link rel="stylesheet" href="css/styleForSignUp.css"> -->
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@1,900&amp;display=swap" rel="stylesheet"> -->
+    <!---- Our own css design --->
+    <link rel="stylesheet" href="css/vendr-animtd-circle.css">
+    <link rel="stylesheet" href="css/progresscircle.css">
+    <!-- <link rel="stylesheet" href="css/style.css"> -->
+    <!-- <link rel="stylesheet" href="<?php bloginfo('stylesheet_url');?>"> -->
+    <link rel="stylesheet" href="css/fontawesome/css/all.css">
+    <?php
+       wp_head();
+    ?>
+
+</head>
+
+<body <?php body_class();?>>
+    <!------ Starting point of Header portion------->
+    <div class="container_full header_bg">
+        <div class="fix container">
+            <header id="header">
+                <div class="logo_left">
+                    <img src="img/logo redesign.png" alt="Image of logo">
+                </div>
+                <nav>
+                    <div id="menu">
+                        <ul>
+                            <li class="active"><a href="#">Home</a></li>
+                            <li><a href="#">Services &rsaquo;</a>
+                                <ul>
+                                    <li><a href="#">Adimn Support &rsaquo;</a></li>
+                                    <li><a href="#">Digital Marketing &rsaquo;</a></li>
+                                    <li><a href="#">Web Design Services</a></li>
+                                    <li><a href="#">Garphics Design Services</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="#">Our products &rsaquo;</a>
+                                <ul>
+                                    <li><a href="#">Graphics Design</a></li>
+                                    <li><a href="#">Wordpress Theme</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="#">Contact us</a></li>
+                            <li>
+                                <div id="sign_in">
+                                    <button id="dialogop3">
+                                        sign in
+                                    </button>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            </header>
+        </div>
+    </div>
+    <!----------- Header portion finish--------->
+    <!----------For testing purpose ----->
