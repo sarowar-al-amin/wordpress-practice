8b7d4c15b4b78c84d9a35c905c43d5599ddc2741
sarowar-al-amin <sajib.sarowaralamin123@gmail.com>
2020-12-28 12:22:14 +0600

    splitting  footer part

--- footer.php
+++ footer.php
@@ -1,7 +1,26 @@
-<!-------------
- This code is written by Md. Sarowar Al-Amin
----------->
-    
+    <!----------- Initial point of nineth container portion --------->
+    <div class="container_full nineth_bg maintainSpace">
+        <div class="fix container">
+            <div class="circle_wrapper">
+                <div class="circle">
+                    <div class="circlechart" data-percentage="95"></div>
+                    <h5>Client satisfiction</h5>
+                    <h6>It is a long established fact that a reader will be distracted</h6>
+                </div>
+                <div class="circle">
+                    <div class="circlechart" data-percentage="90"></div>
+                    <h5>Total Complete Order</h5>
+                    <h6>It is a long established fact that a reader will be distracted</h6>
+                </div>
+                <div class="circle">
+                    <div class="circlechart" data-percentage="20"></div>
+                    <h5>Active Order</h5>
+                    <h6>It is a long established fact that a reader will be distracted</h6>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!----------- Ending point of nineth container portion --------->
     <!------Initaial point of footer part---->
     <div class="container_full footer_bg maintainSpace">
         <div class="fix container footer_head">
