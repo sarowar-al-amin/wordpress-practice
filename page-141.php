8feaa63c0872fcfa6fdbad078242006623816d52
sarowar-al-amin <sajib.sarowaralamin123@gmail.com>
2020-12-28 17:00:13 +0600

    Contact page uploaded

---
+++ page-141.php
@@ -0,0 +1,164 @@
+<?php get_template_part('header');?>
+
+    <!----------- Initial point of Page Marker --------->
+    <div class="container_full">
+        <div class="fix lead_head_pic">
+            <div class="lead_background">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3593.4001267992658!2d89.25256771449587!3d25.757344115068406!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39e32e07d95ffbeb%3A0x6a0ad431db0c318f!2sMaster%20Para%20Rd%2C%20Rangpur!5e0!3m2!1sen!2sbd!4v1608187612279!5m2!1sen!2sbd"
+                    frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+                <!-- <img src="img/pexels-magda-ehlers-3873647.png" alt="Backgound Image of Graphics design"> -->
+            </div>
+            <div class="fix container">
+                <h3>Contact us</h3>
+            </div>
+        </div>
+    </div>
+    <!----------- Ending point of Page Marker --------->
+
+
+    <!----------- Left portion of the contact us --->
+    <!----------- Initial point of contact part -->
+    <div class="container_full">
+        <div class="fix container">
+            <!-----------Left part start here -->
+            <form action="">
+                <div class="left_Of_contact">
+                    <div class="name">
+                        <input type="text" placeholder="Enter you name">
+                    </div>
+                    <div class="enter_email">
+                        <input type="email" placeholder="Enter your email">
+                    </div>
+                    <div class="message_box">
+                        <textarea name="Text-Box" placeholder="Write you message"></textarea>
+                    </div>
+                    <div class="submit_botton">
+                        <input type="submit" value="Submit">
+                    </div>
+                </div>
+            </form>
+            <!---------Right part start here -->
+            <div class="right_Of_contact">
+                <h3>Contact With Us!</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eius mod tempor incididunt ut labore et dolore magna null amet aliqua. Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                <div class="addresses">
+                    <div class="address">
+                        <h5>8455 East River Street</h5>
+                        <h5>Brooklyn, New York 11224</h5>
+                        <h6>itmarket24@example.com</h6>
+                        <h6>+10243 312 123</h6>
+                    </div>
+                    <div class="address">
+                        <h5>8455 East River Street</h5>
+                        <h5>Brooklyn, New York 11224</h5>
+                        <h6>itmarket24@example.com</h6>
+                        <h6>+10243 312 123</h6>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+    <!----------- Ending point of contact part -->
+
+
+    <!------Initaial point of footer part---->
+    <div class="container_full footer_bg maintainSpace">
+        <div class="fix container footer_head">
+
+            <div class="footer_col">
+                <div class="bar_wrap">
+                    <div class="f_bar_y"></div>
+                    <div class="f_bar_b"></div>
+                </div>
+                <br>
+                <h3>About site</h3>
+                <h2>Name and logo</h2>
+                <div class="footer_p">
+                    <p>Lorem Ipsum is simply dummy text of the printing and industry. Lorem Ipsum has been the industry's
+                    </p>
+                    <!-------I don't have any idea is it botton or link------->
+                    <a href="#">Read more</a>
+                </div>
+                <input type="email" placeholder="Enter your email">
+            </div>
+            <div class="footer_col">
+                <div class="bar_wrap">
+                    <div class="f_bar_y"></div>
+                    <div class="f_bar_b"></div>
+                </div>
+                <br>
+                <h3>Our Services</h3>
+                <li><a href="#">Data entry</a></li>
+                <li><a href="#">Graphics design</a></li>
+                <li><a href="#">SEO</a></li>
+                <li><a href="#">Skip Tracing</a></li>
+                <li><a href="#">Lead generation</a></li>
+            </div>
+
+            <div class="footer_col">
+                <div class="bar_wrap">
+                    <div class="f_bar_y"></div>
+                    <div class="f_bar_b"></div>
+                </div>
+                <br>
+                <h3>Quick links</h3>
+                <li><a href="#">About us</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">Contact us</a></li>
+                <li><a href="#">Refund Policy</a></li>
+            </div>
+            <div class="footer_col">
+                <div class="bar_wrap">
+                    <div class="f_bar_y"></div>
+                    <div class="f_bar_b"></div>
+                </div>
+                <br>
+                <h3>Contact Us</h3>
+                <p><i class="fas fa-map-marker-alt"></i> Master Para, Chartolarmor, Rangpur</p>
+                <p><i class="fas fa-envelope"></i> adimin@admin.com</p>
+                <p><i class="fas fa-phone-alt"></i> +0123456789</p>
+            </div>
+
+        </div>
+        <!------Bottom part of the footer------>
+        <div class="white_bar_at_footer"></div>
+        <div class="fix container">
+            <div id="footer_bottom">
+                <p>ItMarket24 &copy; All Reaserved</p>
+                <p>Privacy Terms and Conditions Contact</p>
+                <span>
+                    <a href="#">
+                        <i class="fab fa-facebook"></i>
+                    </a>
+                </span>
+                <span>
+                    <a href="#">
+                        <i class="fab fa-twitter"></i>
+                    </a>
+                </span>
+                <span>
+                    <a href="#">
+                        <i class="fab fa-linkedin"></i>
+                    </a>
+                </span>
+                <span>
+                    <a href="#">
+                        <i class="fab fa-pinterest"></i>
+                    </a>
+                </span>
+            </div>
+        </div>
+
+    </div>>
+
+    <!------ Ending point of footer------>
+
+    <!---------- JavaScript --------->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="jss/vendor-animated-circle.js"></script>
+    <script src="jss/main.js"></script>
+
+</body>
+
+</html>
