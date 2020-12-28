a96e7074680d939a1efd717fbc791e2d346369ea
sarowar-al-amin <sajib.sarowaralamin123@gmail.com>
2020-12-28 12:22:43 +0600

    updated functions.php

--- functions.php
+++ functions.php
@@ -1,6 +1,8 @@
 <?php
     function calling_resources(){
-        wp_enqueue_style('style', get_stylesheet_uri());
+        wp_enqueue_style('mainstyle', get_stylesheet_uri(), ' ', '0.1.0');
+        wp_enqueue_style('progressive-circle-style', get_template_directory_uri().'/css/progresscircle.css', '', '0.1.0');
+
     }
 
     add_action('wp_enqueue_scripts', 'calling_resources');
