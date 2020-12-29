75883eb5379f7040aaca96467745a09215dcddcd
sarowar-al-amin <sajib.sarowaralamin123@gmail.com>
2020-12-29 18:34:10 +0600

    Completing checkout page

---
+++ page-163.php
@@ -0,0 +1,114 @@
+<?php get_template_part('header');?>
+    <!----------- Initial point of Page Marker --------->
+    <div class="container_full">
+        <div class="fix graphics_head_pic">
+            <div class="graphics_background" style="background-image: url('<?php echo get_template_directory_uri();?>/img/leadgeneration_bg.png');">
+                <!-- <img src="img/top-view-tools-marketing.png" alt="Lead generation lead_background Image"> -->
+            </div>
+            <div class="container" style="opacity: 1;">
+                <h3>Lead Generation</h3>
+                <h6>Home > Lead Generation</h6>
+            </div>
+        </div>
+
+    </div>
+    <!----------- Ending point of Page Marker --------->
+
+    <!---------- Initial point of lead generation upload file---->
+    <div class="container_full">
+        <div class="fix container">
+            <div class="upload_lead_generation_file_head">
+                <div class="upload_file">
+                    <h3>Upload File</h3>
+                    <div class="step_bar">
+                        <div class="bar1"></div>
+                        <h6>Upload file</h6>
+                    </div>
+                    <div class="step_bar">
+                        <div class="bar_ashes"></div>
+                        <h6>Map Field</h6>
+                    </div>
+                    <div class="step_bar">
+                        <div class="bar_ashes"></div>
+                        <h6>Check out</h6>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-----------Ending point of lead generation upload file ---->
+
+    <!------------------Initializing Upload point ------>
+    <div class="container_full">
+        <div class="fix container">
+            <div class="lead_generation_upload_point">
+                <div class="upload_point">
+                    <div class="cloud_image">
+                        <a href="#" title="upload from cloud" target="">
+                            <img src="<?php echo get_template_directory_uri();?>/img/Group 4650.png" alt="Cloud Image">
+                        </a>
+                    </div>
+                    <div class="text">
+                        <h4>Upload Your File Here or</h4>
+                    </div>
+                    <div class="input_file">
+                        <input type="file">
+                    </div>
+                    <div class="text">
+                        <h6>Accepted File Type: CSV</h6>
+                        <span><i class="fas fa-sticky-note"></i> file.csv <br><i class="fas fa-sticky-note"></i> file.csv </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!------------------Ending Upload point ------>
+
+    <!------------------Initializing requirement point ------>
+    <div class="container_full">
+        <div class="fix container">
+            <div class="requirement_leadgeneration">
+                <div class="upload_file">
+                    <h3>Requirement</h3>
+                    <div class="step_bar">
+                        <div class="bar1"></div>
+                        <h6>Upload file</h6>
+                    </div>
+                    <div class="step_bar">
+                        <div class="bar1"></div>
+                        <h6>Map Field</h6>
+                    </div>
+                    <div class="step_bar">
+                        <div class="bar_ashes"></div>
+                        <h6>Check out</h6>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!------------------Ending requirement point ------>
+
+    <!------------- initialize  Text area ---->
+    <div class="container_full">
+        <div class="fix container">
+            <dix class="text_box">
+                <textarea name="Write you requirement" cols="100" rows="10" placeholder="Write you own requirement"></textarea>
+            </dix>
+        </div>
+    </div>
+
+    <!------------- Ending text area ----->
+
+    <!--------- Initialize Continue botton ----->
+    <div class="container_full">
+        <div class="fix container">
+            <div class="continue">
+                <!-- <h4>Continue</h4> -->
+                <a href="http://localhost/ExampleSite/check-out/#">Continue</a>
+            </div>
+        </div>
+    </div>
+    <!----------Ending continue botton ------>
+
+    <?php get_template_part('footer');?>
+
