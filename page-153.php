2afd5e6170d862cf83fa5f0b31b52db0817b239f
sarowar-al-amin <sajib.sarowaralamin123@gmail.com>
2020-12-29 12:47:32 +0600

    Web design product page

---
+++ page-153.php
@@ -0,0 +1,156 @@
+<?php get_template_part('header');?>
+
+    <!----------- Initial point of Page Marker --------->
+    <div class="container_full">
+        <div class="fix graphics_head_pic">
+            <div class="graphics_background" style="background-image: url('<?php echo get_template_directory_uri();?>/img/bg_graphic.png');">
+                <!-- <img src="img/top-view-tools-marketing.png" alt="Lead generation lead_background Image"> -->
+            </div>
+            <div class="container" style="opacity: 1;">
+                <h3>Web Design Services</h3>
+                <h6>Home > Web Design Services</h6>
+            </div>
+        </div>
+
+    </div>
+    <!----------- Ending point of Page Marker --------->
+
+    <!------------ First content of graphics design ------->
+    <!------------ Initial point of first content ------->
+    <div class="container_full">
+        <div class="fix container">
+            <div class="graphic_first_contain">
+                <h3>Wordpress Theme</h3>
+                <div class="description">
+                    <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem.</p>
+                </div>
+                <div class="piority">
+                    <label>
+                        <select>
+                            <option selected>Popular</option>
+                            <option>low to high price</option>
+                            <option>high to low price</option>
+                            <option>uploaded date</option>
+                        </select>
+                    </label>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!------------ Ending point of first content ------->
+    <!------------ First content of graphics design ------->
+
+    <!------------ Thrid content of graphics design ------->
+    <!------------ Initial content of graphics design ------->
+    <div class="container_full">
+        <div class="fix container">
+            <div class="fix thrid_graphics_contain">
+                <div class="options_view">
+                    <img src="<?php echo get_template_directory_uri();?>/img/1.png" alt="Image">
+                    <h3>New Concept Real Estate Web</h3>
+                    <h5>Released On: 24 April, 2020</h5>
+                    <h4>Version: 1.0</h4>
+                    <div class="price_tag">
+                        <a href="#">$35</a>
+                    </div>
+                </div>
+                <div class="options_view">
+                    <img src="<?php echo get_template_directory_uri();?>/img/1.png" alt="Image">
+                    <h3>New Concept Real Estate Web</h3>
+                    <h5>Released On: 24 April, 2020</h5>
+                    <h4>Version: 1.0</h4>
+                    <div class="price_tag">
+                        <a href="#">$35</a>
+                    </div>
+                </div>
+                <div class="options_view">
+                    <img src="<?php echo get_template_directory_uri();?>/img/1.png" alt="Image">
+                    <h3>New Concept Real Estate Web</h3>
+                    <h5>Released On: 24 April, 2020</h5>
+                    <h4>Version: 1.0</h4>
+                    <div class="price_tag">
+                        <a href="#">$35</a>
+                    </div>
+                </div>
+                <div class="options_view">
+                    <img src="<?php echo get_template_directory_uri();?>/img/1.png" alt="Image">
+                    <h3>New Concept Real Estate Web</h3>
+                    <h5>Released On: 24 April, 2020</h5>
+                    <h4>Version: 1.0</h4>
+                    <div class="price_tag">
+                        <a href="#">$35</a>
+                    </div>
+                </div>
+                <div class="options_view">
+                    <img src="<?php echo get_template_directory_uri();?>/img/1.png" alt="Image">
+                    <h3>New Concept Real Estate Web</h3>
+                    <h5>Released On: 24 April, 2020</h5>
+                    <h4>Version: 1.0</h4>
+                    <div class="price_tag">
+                        <a href="#">$35</a>
+                    </div>
+                </div>
+                <div class="options_view">
+                    <img src="<?php echo get_template_directory_uri();?>/img/1.png" alt="Image">
+                    <h3>New Concept Real Estate Web</h3>
+                    <h5>Released On: 24 April, 2020</h5>
+                    <h4>Version: 1.0</h4>
+                    <div class="price_tag">
+                        <a href="#">$35</a>
+                    </div>
+                </div>
+                <div class="options_view">
+                    <img src="<?php echo get_template_directory_uri();?>/img/1.png" alt="Image">
+                    <h3>New Concept Real Estate Web</h3>
+                    <h5>Released On: 24 April, 2020</h5>
+                    <h4>Version: 1.0</h4>
+                    <div class="price_tag">
+                        <a href="#">$35</a>
+                    </div>
+                </div>
+                <div class="options_view">
+                    <img src="<?php echo get_template_directory_uri();?>/img/1.png" alt="Image">
+                    <h3>New Concept Real Estate Web</h3>
+                    <h5>Released On: 24 April, 2020</h5>
+                    <h4>Version: 1.0</h4>
+                    <div class="price_tag">
+                        <a href="#">$35</a>
+                    </div>
+                </div>
+                <div class="options_view">
+                    <img src="<?php echo get_template_directory_uri();?>/img/1.png" alt="Image">
+                    <h3>New Concept Real Estate Web</h3>
+                    <h5>Released On: 24 April, 2020</h5>
+                    <h4>Version: 1.0</h4>
+                    <div class="price_tag">
+                        <a href="#">$35</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!------------ Ending point of Third content ------->
+    <!------------ Third  content of graphics design ------->
+
+    <!--------Initial point of sixth container of Graphics ----------->
+    <div class="container_full fix lead_sixth_contain maintainSpace">
+        <div class="lead_six_left_img">
+            <img src="<?php echo get_template_directory_uri();?>/img/Path 2197.png" alt="Left side Image">
+        </div>
+        <div class="lead_six_right_img">
+            <img src="<?php echo get_template_directory_uri();?>/img/Path 312.png" alt="Right side Image">
+        </div>
+        <div class="fix container">
+            <div class="lead_six_text">
+                <h4>We're not your typical Digital Marketing Agency</h4>
+                <div class="contact_us_box">
+                    <a href="#">Contact with us</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--------Ending point of sixth container of Graphics ----------->
+
+
+    <?php get_template_part('footer');?>
