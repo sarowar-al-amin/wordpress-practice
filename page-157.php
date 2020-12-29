df3fef249e83bd879bcb9f2060297cc0fff3b11b
sarowar-al-amin <sajib.sarowaralamin123@gmail.com>
2020-12-29 13:26:26 +0600

    web design  products page

---
+++ page-157.php
@@ -0,0 +1,181 @@
+<?php get_template_part('header');?>
+
+    <!----------- Initial point of Page Marker --------->
+    <div class="container_full">
+        <div class="fix graphics_head_pic">
+            <div class="graphics_background" style="background-image: url('<?php echo get_template_directory_uri();?>/img/bg_graphic.png');">
+                <!-- <img src="img/top-view-tools-marketing.png" alt="Lead generation lead_background Image"> -->
+            </div>
+            <div class="container" style="opacity: 1;">
+                <h3>Graphics design product lists</h3>
+                <h6>Home > graphics design</h6>
+            </div>
+        </div>
+
+    </div>
+    <!----------- Ending point of Page Marker --------->
+    <!------------------------------------------------->
+
+    <!----### First container of graphics design product list ###---->
+    <div class="container_full maintainSpace">
+        <div class="fix container">
+            <!------***Left side of graphics design product list***-->
+            <div class="left_graph_product">
+                <div class="preview_image">
+                    <img src="<?php echo get_template_directory_uri();?>/img/Preview Image.png" alt="Preview Image">
+                </div>
+                <div class="documentation">
+                    <a href="#">Documentation</a>
+                </div>
+                <!------ Help bar ---->
+                <div class="help_bar">
+                    <div class="image">
+                        <img src="<?php echo get_template_directory_uri();?>/img/Path 1544.png" alt="Side bar Image">
+                    </div>
+                    <div class="help_box">
+                        <img src="<?php echo get_template_directory_uri();?>/img/Group 4608.png" alt="Image">
+                        <h4>Need Any Kind Of Support?</h4>
+                        <span><i class="far fa-envelope"></i> sarowaralamin@outlook.com <br> <i class="fas fa-phone-alt"></i> +0123456789</span>
+                    </div>
+                    <div class="support_hour">
+                        <img src="<?php echo get_template_directory_uri();?>/img/schedule-24px.png" alt="Watch Image">
+                        <h3>20 Hours Real Time Support</h3>
+                    </div>
+                    <div class="image">
+                        <img src="<?php echo get_template_directory_uri();?>/img/Path 2539.png" alt="Image">
+                    </div>
+                </div>
+                <!--------Product---->
+                <div class="product">
+                    <a href="#">
+                        <img src="<?php echo get_template_directory_uri();?>/img/1.png" alt="product image">
+                    </a>
+                </div>
+                <div class="product">
+                    <a href="#">
+                        <img src="<?php echo get_template_directory_uri();?>/img/4.png" alt="product image">
+                    </a>
+                </div>
+                <div class="product">
+                    <a href="#">
+                        <img src="<?php echo get_template_directory_uri();?>/img/5.png" alt="product image">
+                    </a>
+                </div>
+                <!----App creator portion-->
+                <div class="app_creators">
+                    <h3>Apps Creator Web Landing Page</h3>
+                    <ul>
+                        <li>
+                            <a href="#"><i class="fas fa-circle"></i> 10 Home Pages (Multi Pages).</a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="fas fa-circle"></i> 5 Header styles can do 10+ header variations.</a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="fas fa-circle"></i> 4 Footer styles can do different variations.</a>
+                        </li>
+
+                        <li>
+                            <a href="#"><i class="fas fa-circle"></i> 3 Blog layouts.</a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="fas fa-circle"></i>SEO Friendly</a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="fas fa-circle"></i> 7 Portfolios Layouts (03 Grid, 02 Isotope & 02 Masonry).</a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="fas fa-circle"></i> Google Web Font</a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="fas fa-circle"></i> Free Lifetime updates!</a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="fas fa-circle"></i> Clean, Trending and Modern Design.</a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="fas fa-circle"></i> Dynamic Page Header</a>
+                        </li>
+                    </ul>
+                </div>
+                <!--------Icons portion ---->
+                <div class="icons_graph">
+                    <h3>ICONS: </h3>
+                    <li>Font awsome icons-<a href="https://fortawesome.github.io/Font-Awesome" target="_blank">https://fortawesome.github.io/Font-Awesome/</a></li>
+                    <li>Flat Icon –
+                        <a href="www.flaticon.com" target="_blank">www.flaticon.com</a>
+                    </li>
+                </div>
+
+                <!------Images portion ---->
+                <div class="images_graphic">
+                    <h3>IMAGES: </h3>
+                    <li>
+                        <a href="http://photodune.net/" target="_blank">http://photodune.net/</a>
+                    </li>
+                    <li><a href="http://allfreephotos.net/" target="_blank">http://allfreephotos.net/</a></li>
+                </div>
+                <!------Fonts portion ----->
+                <div class="fonts_graphic">
+                    <h3>FONTS: </h3>
+                    <li>
+                        Fonts
+                        <a href="https://www.google.com/fonts/specimen/roboto" target="_blank">https://www.google.com/fonts/specimen/roboto</a></li>
+                </div>
+            </div>
+            <!--------Right portion of graphics product page-->
+            <div class="right_graph_product">
+                <div class="upper_box">
+                    <h2>Purchase This Product</h2>
+                    <div class="type_tag">
+                        <h3>Regular</h3>
+                        <h3>$35</h3>
+                    </div>
+                    <div class="note">
+                        <p>
+                            <p>Use, by you or one client, in a single end product which end users are not charged for.</p>
+                        </p>
+                    </div>
+                    <div class="note">
+                        <p>
+                            <p>Price is in US dollars. Price displayed excludes sales tax.</p>
+                        </p>
+                    </div>
+
+                    <div class="license_botton">
+                        <a href="#"><i class="fas fa-shopping-cart"></i> Purchase a License</a>
+                    </div>
+                    <div class="buy2_botton">
+                        <a href="#">Buy now</a>
+                    </div>
+                </div>
+                <!-------Lower box------>
+                <div class="lower_box">
+                    <h2>Information</h2>
+                    <h3><strong> Released On : </strong>24 April, 2020</h3>
+                    <h3><strong>Last Update : </strong>24 April, 2020</h3>
+                    <h3><strong>Version :</strong> 1.0</h3>
+                    <h3><strong>Framework:</strong> Underscore</h3>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--------Initial point of sixth container of leadgeneration ----------->
+    <div class="container_full fix lead_sixth_contain maintainSpace">
+        <div class="lead_six_left_img">
+            <img src="<?php echo get_template_directory_uri();?>/img/Path 2197.png" alt="Left side Image">
+        </div>
+        <div class="lead_six_right_img">
+            <img src="<?php echo get_template_directory_uri();?>/img/Path 312.png" alt="Right side Image">
+        </div>
+        <div class="fix container">
+            <div class="lead_six_text">
+                <h4>We're not your typical Digital Marketing Agency</h4>
+                <div class="contact_us_box">
+                    <a href="#">Contact with us</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--------Ending point of sixth container of leadgeneration ----------->
+    <?php get_template_part('footer');?>
