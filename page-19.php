496b559b9a0a07af14c1847ee22ae3b5afcc83a7
sarowar-al-amin <sajib.sarowaralamin123@gmail.com>
2020-12-30 17:06:04 +0600

    updated other pages

--- page-19.php
+++ page-19.php
@@ -25,129 +25,30 @@
                     <img src="<?php echo get_template_directory_uri();?>/img/leadGeneration1st.png" alt="Image of people">
                 </div>
             </section>
-            <section class="graphics_right_side_content">
-                <p>
-                    Lead Generation describes the Advertising and Marketing method of stimulating and capturing activity in a product or carrier for the reason of growing a sales pipeline.
-                </p>
-                <p>Lead Generation frequently makes use of Digital Channels and has been undergoing enormous adjustments in the latest years from the rise of new online and social techniques. In particular, the abundance of facts without difficulty on hand
-                    online has led to the rise of the emergence of new strategies to improve and qualify possible leads earlier than passing them to sales.
-                </p>
-                <p>
-                    <mark>Itmarket24.com </mark>is ready to help you with the skip tracing Service.
-                </p>
-            </section>
+            <?php get_template_part('first_container_text');?>
         </div>
     </div>
     <!----------- Ending point of First container --------->
+
+    <!----------- Initial point of Second container ------->
     <?php get_template_part('second_container');?>
     <!----------- Ending point of Second container ------->
 
     <!----------- Initial  point of third container ------->
-    <div class="container_full maintainSpace">
-        <div class="fix container">
-            <div class="graphics_third_contain">
-                <div class="head_bar">
-                    <div class="bar">
-                        <div class="yellow"></div>
-                        <div class="blue"></div>
-                    </div>
-                </div>
-                <br>
-                <div class="info_text">
-                    <h2>Customers Can Get:</h2>
-                    <h3>Decision Makers:(silver package)</h3>
-                    <p>
-                        Owner/Investor<i class="fas fa-hand-point-right"></i><i class="fas fa-hand-point-right"></i>Founder/Chairman,
-                        <i class="fas fa-hand-point-right"></i><i class="fas fa-hand-point-right"></i> C-level/ Board Directors <i class="fas fa-hand-point-right"></i><i class="fas fa-hand-point-right"></i> HR Directors <i class="fas fa-hand-point-right"></i>
-                        <i class="fas fa-hand-point-right"></i>SVP/EVP <i class="fas fa-hand-point-right"></i><i class="fas fa-hand-point-right"></i>Marketing Directors/Manager. Education: (silver package)<br> School/Law School /College/University: Principal,
-                        Director, Athlete Director, Teachers, etc.
-                    </p>
-                    <h3>Business:(Gold package)</h3>
-                    <P>
-                        Supplier/Wholesale Distributors of any Products/ Beauty Parlour/ Gym <i class="fas fa-hand-point-right"></i><i class="fas fa-hand-point-right"></i>Stationary Shops/Vitamins Supplement Shops <i class="fas fa-hand-point-right"></i>
-                        <i class="fas fa-hand-point-right"></i>Hotel/restaurant, <i class="fas fa-hand-point-right"></i> <i class="fas fa-hand-point-right"></i>Insurance Company <i class="fas fa-hand-point-right"></i><i class="fas fa-hand-point-right"></i>University
-                        and college data
-                        <i class="fas fa-hand-point-right"></i><i class="fas fa-hand-point-right"></i> Club Data <i class="fas fa-hand-point-right"></i><i class="fas fa-hand-point-right"></i>Carpet Cleaning Businesses <i class="fas fa-hand-point-right"></i>
-                        <i class="fas fa-hand-point-right"></i>Payroll Company <i class="fas fa-hand-point-right"></i><i class="fas fa-hand-point-right"></i>HVAC/Air-condition Company <i class="fas fa-hand-point-right"></i><i class="fas fa-hand-point-right"></i>domiciliary/home
-                        care Company
-                    </P>
-                    <h3>Professional:(platinum package)</h3>
-                    <p>
-                        Law Firms/Lawyers, Accounting Firms/Accountants, Realtor/Real Estate Firms, Health Care Clinics/Doctors, Dental service, Entertainers, Physical Therapist data, CBD Companies, etc.
-                        <br> Feel free to contact <em>itmarket24.com</em> for your service. We are committed to providing the best Lead Generation Service.
-                    </p>
-                </div>
-            </div>
-        </div>
-    </div>
+    <?php get_template_part('package_description');?>
     <!----------- Ending point of third container ------->
 
     <!--------Initial point of fourth container/ package portion ----------->
     <?php get_template_part('packages');?>
     <!--------Ending point of fourth container/ package portion ----------->
+
     <!-------- Initial point of fifth container of leadgeneration ----------->
-    <div class="container_full maintainSpace">
-        <div class="fix container">
-            <div class="lead_fifth_contain">
-                <div class="lead_fifth_top">
-                    <div class="head_bar">
-                        <div class="bar">
-                            <div class="yellow"></div>
-                            <div class="blue"></div>
-                        </div>
-                    </div>
-                    <br>
-                    <h2>Our step by step process</h2>
-                    <p>
-                        It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.
-                    </p>
-                </div>
-                <!---------Box portion of fifth container of lead generation-->
-                <div class="bottom_lead_fifth">
-                    <div class="operation_part">
-                        <img src="<?php echo get_template_directory_uri();?>/img/Group 4375.png" alt="Image of first step">
-                        <h4>Upload file</h4>
-                        <p>It is a long established fact that a reader will be distracted by the readable </p>
-                    </div>
-                    <div class="operation_part">
-                        <img src="<?php echo get_template_directory_uri();?>/img/Group 4390.png" alt="Image of second step">
-                        <h4>Requirement</h4>
-                        <p>It is a long established fact that a reader will distracted the readable content of a page looking at its layout.</p>
-                    </div>
-                    <div class="operation_part">
-                        <img src="<?php echo get_template_directory_uri();?>/img/Group 4392.png" alt="Image of third step">
-                        <h4>Check out</h4>
-                        <p>It is a long established fact that a reader will distracted the readable content</p>
-                    </div>
-                    <div class="operation_part">
-                        <img src="<?php echo get_template_directory_uri();?>/img/Group 4394.png" alt="Image of fourth step">
-                        <h4>Download</h4>
-                        <p>It is a long established fact that a reader will be distracted by the readable content of a page when </p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    <?php get_template_part('fifth_container');?>
     <!--------Ending point of fifth container of leadgeneration ----------->
 
     <!--------Initial point of sixth container of leadgeneration ----------->
-    <div class="container_full fix lead_sixth_contain">
-        <div class="lead_six_left_img">
-            <img src="<?php echo get_template_directory_uri();?>/img/Path 2197.png" alt="Left side Image">
-        </div>
-        <div class="lead_six_right_img">
-            <img src="<?php echo get_template_directory_uri();?>/img/Path 312.png" alt="Right side Image">
-        </div>
-        <div class="fix container">
-            <div class="lead_six_text">
-                <h4>We're not your typical Digital Marketing Agency</h4>
-                <div class="contact_us_box">
-                    <a href="#">Contact with us</a>
-                </div>
-            </div>
-        </div>
-    </div>
+    <?php get_template_part('sixth_container');?>
     <!--------Ending point of sixth container of leadgeneration ----------->
+
+
     <?php get_template_part('footer');?>
-
-
